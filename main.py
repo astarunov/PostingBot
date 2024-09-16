e67646da2e2b6eb5546931e885972fa78fbd9765
astarunov <astarunov444@gmail.com>
2024-09-16 17:30:20 +0200

    set git

---
+++ main.py
@@ -0,0 +1,14 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from Handlers import
+
+async def main():
+    bot = Bot("|", parse_mode="markdown")
+    dp = Dispatcher()
+    dp.include_routers(
+
+    )
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+if __name__ == '__main__':
+    asyncio.run(main())
